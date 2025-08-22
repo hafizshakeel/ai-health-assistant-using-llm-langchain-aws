0c10625cf27ca914c3b4524735a46ba64ccde37c
hafizshakeel <hafizshakeel1997@gmail.com>
2025-08-23 01:19:59 +0900

    Add project dependencies and setup configuration

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot_app",
+    version="0.0.1",
+    author="Hafiz Shakeel Ahmad",
+    author_email="hafizshakeel1997@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+) 
