f2297928d0967f584d36a9bc4cc387bc07ccc325
hafizshakeel <hafizshakeel1997@gmail.com>
2025-08-23 01:18:49 +0900

    Initialize project structure with directory setup script

---
+++ template.sh
@@ -0,0 +1,18 @@
+# create directories
+mkdir -p src 
+mkdir -p research
+
+# create files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch main.py
+touch research/notes.ipynb
+touch requirements.txt
+
+
+echo "Files created successfully!"
+  
